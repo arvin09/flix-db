e4cdcd45cd63cbc3d86b1a613c90e1c1d1d9d70c
Arvin <arvind.waghmare@morningstar.com>
2020-01-16 11:04:30 +0530

    added NavItem

--- src/components/NavItems.vue
+++ src/components/NavItems.vue
@@ -8,13 +8,16 @@
       data-toggle="dropdown"
       aria-haspopup="true"
       aria-expanded="false"
-      >{{ item }}</a
+      >{{ getUpperCaseMenuName }}</a
     >
     <div class="dropdown-menu" aria-labelledby="moiveDropdown">
-      <!-- <template v-for="subItem in subItems" :key="$index">
-        <a class="dropdown-item" href="#">{{subItem}}</a>
-        <div class="dropdown-divider"></div>
-      </template> -->
+      <a
+        v-for="(subItem, index) in subItems"
+        :key="index"
+        class="dropdown-item"
+        href="#"
+        >{{ subItem }}</a
+      >
     </div>
   </li>
 </template>
@@ -22,7 +25,7 @@
 export default {
   name: "nav-item",
   props: {
-    item: {
+    menuItem: {
       type: String,
       default: ""
     },
@@ -31,8 +34,65 @@ export default {
       default: () => []
     }
   },
-  data() {
-    return {};
+  computed: {
+    getUpperCaseMenuName() {
+      return this.menuItem.toString().toUpperCase();
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+@media screen and (min-width: 768px) {
+  .dropdown:hover .dropdown-menu,
+  .btn-group:hover .dropdown-menu {
+    display: block;
+  }
+  .dropdown-menu {
+    margin-top: 0;
+  }
+  .dropdown-toggle {
+    margin-bottom: 2px;
+  }
+  .navbar .dropdown-toggle,
+  .nav-tabs .dropdown-toggle {
+    margin-bottom: 0;
+  }
+}
+
+.dropdown-menu {
+  width: 500px;
+  background: transparent;
+  border: 0;
+}
+.dropdown-menu a {
+  display: inline-block;
+}
+
+.nav-item > a {
+  padding-top: 0;
+  padding-bottom: 0;
+  padding-right: 40px !important;
+}
+
+.nav-link {
+  font-weight: bold;
+}
+
+.dropdown-toggle::after {
+  display: none;
+}
+
+.dropdown-item {
+  width: auto;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+
+.dropdown-item:hover {
+  background-color: transparent;
+  color: red;
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -18,57 +18,13 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="moiveDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              >MOVIES</a
-            >
-            <div class="dropdown-menu" aria-labelledby="moiveDropdown">
-              <a class="dropdown-item" href="#">Popular</a>
-              <a class="dropdown-item" href="#">Top Rated</a>
-              <a class="dropdown-item" href="#">Up Coming</a>
-              <a class="dropdown-item" href="#">Now Playing</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="tvShowsDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              >TV SHOWS</a
-            >
-            <div class="dropdown-menu" aria-labelledby="tvShowsDropdown">
-              <a class="dropdown-item" href="#">Popular</a>
-              <a class="dropdown-item" href="#">Top Rated</a>
-              <a class="dropdown-item" href="#">ON TV</a>
-              <a class="dropdown-item" href="#">Aired Today</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="tvShowsDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              >PEOPLE</a
-            >
-            <div class="dropdown-menu" aria-labelledby="tvShowsDropdown">
-              <a class="dropdown-item" href="#">Popular People</a>
-            </div>
-          </li>
+          <nav-item
+            v-for="(item, index) in menu"
+            :key="index"
+            :menuItem="menu[index].name"
+            :subItems="menu[index].subItems"
+          >
+          </nav-item>
         </ul>
       </div>
 
@@ -116,60 +72,34 @@
     </div>
   </div>
 </template>
+<script>
+import navItem from "@/components/NavItems.vue";
+export default {
+  components: {
+    navItem
+  },
+  data() {
+    return {
+      menu: [
+        {
+          name: "Movie",
+          subItems: ["Popular", "Top Rated", "Up Coming", "Now Playing"]
+        },
+        {
+          name: "Tv Shows",
+          subItems: ["Popular", "Top Rated", "On TV", "Aired Today"]
+        },
+        {
+          name: "People",
+          subItems: ["Popular People"]
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <style lang="scss" scoped>
-@media screen and (min-width: 768px) {
-  .dropdown:hover .dropdown-menu,
-  .btn-group:hover .dropdown-menu {
-    display: block;
-  }
-  .dropdown-menu {
-    margin-top: 0;
-  }
-  .dropdown-toggle {
-    margin-bottom: 2px;
-  }
-  .navbar .dropdown-toggle,
-  .nav-tabs .dropdown-toggle {
-    margin-bottom: 0;
-  }
-}
-
-.dropdown-menu {
-  width: 500px;
-  background: transparent;
-  border: 0;
-}
-.dropdown-menu a {
-  display: inline-block;
-}
-
-.nav-item > a {
-  padding-top: 0;
-  padding-bottom: 0;
-  padding-right: 40px !important;
-
-}
-
-.dropdown-toggle::after {
-  display: none;
-}
-
-.dropdown-item {
-  width: auto;
-  color: white;
-  font-weight: bold;
-  font-size: 14px;
-  padding-top: 0;
-  padding-bottom: 0;
-
-}
-
-.dropdown-item:hover {
-  background-color: transparent;
-  color: red;
-}
-
 nav {
   .navbar-brand {
     img {
@@ -178,9 +108,6 @@ nav {
     text-align: left;
   }
 
-  .nav-link {
-    font-weight: bold;
-  }
   height: 110px;
 }
 #search {
