dc044f6bb5b4320d92963c89a3e7607a1e9c0b44
Arvin <arvind.waghmare@morningstar.com>
2020-01-22 22:10:39 +0530

    Updated orignal title to title trimmed synopsis to show 250 chars

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,8 +6,8 @@
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{ details.original_title }}</h5>
-          <p class="card-text">{{ details.overview }}</p>
+          <h5 class="card-title">{{ details.title }}</h5>
+          <p class="card-text">{{ trimSynopsis }}</p>
           <p class="card-text">
             <small class="text-muted">Last updated 3 mins ago</small>
           </p>
@@ -27,10 +27,18 @@ export default {
   },
   data() {
     return {
-      baseImageUrl: "https://image.tmdb.org/t/p/w500"
+      baseImageUrl: "https://image.tmdb.org/t/p/w500",
+      synopsisLength: 250
     };
   },
   computed: {
+    trimSynopsis() {
+      let overview = this.details.overview;
+      if (overview.length > this.synopsisLength) {
+        overview = `${overview.substr(0, this.synopsisLength)} ...`;
+      }
+      return overview;
+    },
     posterLink() {
       return `${this.baseImageUrl}${this.details.poster_path}`;
     }
@@ -38,6 +46,10 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.card {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+}
 .card-body {
   padding: 0.5rem;
   font-size: 14px;
