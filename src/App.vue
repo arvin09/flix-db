11f2194eba4584f7764e47bfd33c1b46e130f6b3
Arvind Waghmare <arvind.waghmare@morningstar.com>
2020-08-02 00:13:52 +0530

    Add footer and make header sticky

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <Navigation></Navigation>
     </header>
     <router-view />
+    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+      <div class="container text-center">
+        <small>Copyright &copy; Your Website</small>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -18,12 +23,17 @@ export default {
 
 <style lang="scss">
 header {
-  position: sticky;
+  position: fixed;
   top: 0;
   z-index: 1;
+  width: 100%;
 }
 #app {
   padding: 0;
   height: 100%;
+}
+
+.container.page-section {
+  margin-top: 110px;
 }
 </style>
